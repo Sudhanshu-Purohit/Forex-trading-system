4f67dff14a5388131cf8b004531380aba6f66bb6
Sudhanshu Purohit <purohitsudhanshu294@gmail.com>
2024-04-06 18:16:02 +0530

    account api's added

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { AccountsService } from './accounts.service';
 
 @Controller('accounts')
-export class AccountsController {}
+export class AccountsController {
+    constructor(private readonly accountsService: AccountsService) { }
+
+    @Get('balance')
+    getBalances() {
+        return this.accountsService.getBalances();
+    }
+
+    @Post('topup')
+    topUpAccount(@Body('currency') currency: string, @Body('amount') amount: number) {
+        return this.accountsService.topUpAccount(currency, amount);
+    }
+}
--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AccountsService {}
+export class AccountsService {
+    private balances: Record<string, number> = { USD: 0, EUR: 0, GBP: 0 };
+
+    getBalances() {
+        return this.balances;
+    }
+
+    topUpAccount(currency: string, amount: number) {
+        if( amount <= 0) {
+            throw new HttpException(`Amount must be a valid number greater than 0`, HttpStatus.BAD_REQUEST);
+        }
+
+        const supportedCurrencies = ["USD", "EUR", "GBP"];
+        if (!supportedCurrencies.includes(currency)) {
+            throw new HttpException(`Currency ${currency} is not supported`, HttpStatus.BAD_REQUEST);
+        }
+
+        this.balances[currency] += amount;
+        return { message: "Account topped up successfully", balances: this.balances };
+    }
+}
